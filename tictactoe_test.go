ddaf8ec3c2200a2ee679bf96f67a14529c1aa786
kurotakazuki <kabisakanakabi@gmail.com>
2023-07-05 02:53:42 +0000

    feat: sub2

--- tictactoe_test.go
+++ tictactoe_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 )
+
 //subtest1のテスト
 func TestPutToken01(t *testing.T) {
 	b := &Board{
@@ -33,6 +34,30 @@ func TestPutToken03(t *testing.T) {
 	b.put(2, 2, "o")
 	if b.get(2, 2) != "o" {
 		t.Error("Test03 is failed.")
+	}
+	b.show()
+	fmt.Print("\n")
+}
+
+//subtest2のテスト
+func TestPutToken04(t *testing.T) {
+	b := &Board{
+		tokens: []int{1, 2, 1, 2, 1, 2, 0, 0, 1},
+	}
+	str := b.judge()
+	if str != "o" {
+		t.Error("Test04 is failed")
+	}
+	b.show()
+	fmt.Print("\n")
+}
+func TestPutToken05(t *testing.T) {
+	b := &Board{
+		tokens: []int{2, 1, 1, 2, 1, 0, 2, 0, 0},
+	}
+	str := b.judge()
+	if str != "x" {
+		t.Error("Test05 is failed")
 	}
 	b.show()
 	fmt.Print("\n")
